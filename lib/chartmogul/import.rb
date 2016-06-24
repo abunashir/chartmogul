0a7f25f78dfb0d46ebdc49335cf6bf7e2804e095
Abu Nashir <abunashir@gmail.com>
2016-06-24 14:13:58 +0700

    Remove importing duplications
    * Remove `list` related duplications
    * Remove `create` related duplications
    * Remove `delete` related duplications
    * Move import sub modules to `import.rb`

---
+++ lib/chartmogul/import.rb
@@ -0,0 +1,9 @@
+require "chartmogul/import/base"
+require "chartmogul/import/plan"
+require "chartmogul/import/customer"
+require "chartmogul/import/data_source"
+
+module Chartmogul
+  module Import
+  end
+end
--- lib/chartmogul/import/customer.rb
+++ lib/chartmogul/import/customer.rb
@@ -1,24 +1,14 @@
 module Chartmogul
   module Import
-    class Customer
-      def self.create(name:, external_id:, data_source_uuid:, **args)
-        required_parameters = {
-          name: name,
-          external_id: external_id,
-          data_source_uuid: data_source_uuid
-        }
+    class Customer < Base
+      private
 
-        Chartmogul.post_resource(
-          "import/customers", required_parameters.merge(args)
-        )
+      def end_point
+        "customers"
       end
 
-      def self.list(options = {})
-        Chartmogul.get_resource "import/customers", options
-      end
-
-      def self.delete(uuid:)
-        Chartmogul.delete_resource ["import/customers", uuid].join("/")
+      def required_keys
+        [:name, :external_id, :data_source_uuid]
       end
     end
   end
